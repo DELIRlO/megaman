201b41d0902e9806a359d4a024e75adec880b82a
CARLOS AUGUSTO DINIZ FILHO <ysneshy@gmail.com>
2025-07-28 11:46:21 -0300

    :white_check_mark: :bug: :rocket: :alien: :robot: bb

---
+++ gitkill.bat
@@ -0,0 +1,9 @@
+:: Versão Windows
+@echo off
+set /p confirm="Confirmar git add/commit/push? (s/n): "
+if "%confirm%"=="s" (
+    git add .
+    git commit -m ":white_check_mark: :bug: :rocket: :alien: :robot: bb"
+    git push origin main
+)
+pause
