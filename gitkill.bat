3cb736267ae50db44976d710e859be2e3845ddd0
CARLOS AUGUSTO DINIZ FILHO <ysneshy@gmail.com>
2025-07-31 13:53:20 -0300

    :white_check_mark: :bug: :rocket: :alien: :robot: att da barra debug

--- gitkill.bat
+++ gitkill.bat
@@ -3,7 +3,7 @@
 set /p confirm="Confirmar git add/commit/push? (s/n): "
 if "%confirm%"=="s" (
     git add .
-    git commit -m ":white_check_mark: :bug: :rocket: :alien: :robot: processando"
+    git commit -m ":white_check_mark: :bug: :rocket: :alien: :robot: att da barra debug"
     git push origin main
 )
 pause
